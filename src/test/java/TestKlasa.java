5e9460db3cae53a77ddf2d7e888c433da44063f8
petarpop88 <petarpop88@gmail.com>
2023-10-26 09:35:24 +0200

    treci commit

---
+++ src/test/java/TestKlasa.java
@@ -0,0 +1,11 @@
+import com.sun.jdi.PathSearchingVirtualMachine;
+
+public class TestKlasa {
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println("fsdfdsdfdfsdfsfddfs");
+    }
+}
